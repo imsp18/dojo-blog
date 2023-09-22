e3c461b44119962ce80bca3f67731ad9561a216e
Shivang Parmar <shivangparmar537@gmail.com>
2023-09-22 22:42:46 +0530

    implemented the concept of Events and useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
-    return ( 
+    //let name = "mario";
+    const [name, setName] = useState('mario');
+    const[age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
+    }
+
+    return (
         <div className="Home">
             <h2>Home Page</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
